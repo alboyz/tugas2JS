b242cc52dc397407ab7ae7b62fba22a7a4abae7e
ali <alboyzskeaters@gmail.com>
2020-01-07 14:37:11 +0700

    Tugas2JS

---
+++ tugas2.js
@@ -0,0 +1,11 @@
+let nama = "Ali";
+let umur =27;
+let jk=true;
+let kosong =null;
+let tidakTahu;
+let buah = ["apel",  "jeruk", "nanas"];
+console.log(nama);
+console.log(umur);
+console.log(jk);
+console.log(tidakTahu);
+console.log(buah);
